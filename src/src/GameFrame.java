6bbf87889b9b03d42e466363afea4a461272ab3b
ReedJordan <89485455+ReedJordan@users.noreply.github.com>
2025-05-16 15:33:39 -0700

    added game frame

--- src/src/GameFrame.java
+++ src/src/GameFrame.java
@@ -1,3 +1,11 @@
 public class GameFrame(){
-
+  public static void main(String[] args){
+        JFrame frame = new JFrame("Snake");
+        GamePanel gamePanel = new GamePanel();
+        frame.add(gamePanel);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 }
