734966aeb81df5de1868e4881c55302fe90acdd4
ReedJordan <89485455+ReedJordan@users.noreply.github.com>
2025-05-20 09:17:49 -0700

    added class

--- src/src/GameFrame.java
+++ src/src/GameFrame.java
@@ -1,5 +1,7 @@
 public class GameFrame(){
   public static void main(String[] args){
+        //added this class in GitHub
+    
         JFrame frame = new JFrame("Snake");
         GamePanel gamePanel = new GamePanel();
         frame.add(gamePanel);
